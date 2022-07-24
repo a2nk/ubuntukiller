87e377ee0734bf7a3768ba0b10186ee99faa0206
Aank is ME <email@aank.me>
2022-07-24 11:32:57 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,3 +3,4 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt-get install ubuntu-desktop xrdp stacer -y
 sudo rm /usr/share/polkit-1/actions/org.freedesktop.color.policy
+sudo systemctl restart xrdp
